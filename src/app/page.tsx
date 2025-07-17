df343aa16f9ab7fc109ba3e2f85dc8ea49a7f11a
ashish2508 <jha250805@gmail.com>
2025-07-17 23:34:41 +0000

    refactor: 🔨 trying out imagekit.io

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-     
-    <div className="bg-black">hello</div>
+    <div className="bg-black">
+      <Image
+        src="https://ik.imagekit.io/jgj9xx779/tr:w-800,h-800,l-text,i-hello,l-end/tr=l-text,i-gay/12.jpeg?updatedAt=1752794741390"
+        alt="meme"
+        width={800}
+        height={800}
+        layout="intrinsic"
+      />
+    </div>
   );
 }
