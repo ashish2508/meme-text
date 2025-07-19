7e3461f9929b904a714a16663f618fc9f5b9e971
ashish2508 <jha250805@gmail.com>
2025-07-19 00:56:19 +0000

    feat: ✨ default truth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import type { AuthResponse } from "@/types/types";
 import { IKImage, IKUpload, ImageKitProvider } from "imagekitio-next";
+import { useState } from "react";
 export default function Home() {
-
+  const [filePath,setFilePath]= useState("");
   const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
   const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_URL;
   const authenticator: () => Promise<AuthResponse> = async () => {
@@ -18,37 +19,41 @@ export default function Home() {
       const token = String(data.token);
 
       return { signature, expire, token };
-    } catch (error:any) {
+    } catch (error: any) {
       throw new Error(`Authentication request failed: ${error.message}`);
     }
   };
   return (
     <div>
       <ImageKitProvider publicKey={publicKey} authenticator={authenticator!} urlEndpoint={urlEndpoint}>
+        {filePath && (
         <IKImage
-          path={"12.jpeg"}
+          path={filePath}
           alt="text"
           width={400}
           height={400}
           transformation={[
             { raw: "l-text,i-didn't post today,fs-20,l-end" },
           ]}
-        />
-        
+        /> 
+        )}
+
         <div>
           <h2>File Upload</h2>
           <IKUpload
-          fileName={"12.jpeg"}
-          onError={(error) => {
-            console.error("Upload error:", error);
-          }}
-          onSuccess={(response) => {
-            console.log("Upload successful", response);
-          }}
-        />
+            fileName={"12.jpeg"}
+            onError={(error) => {
+              console.error("Upload error:", error);
+            }}
+            onSuccess={(response) => {
+              console.log("Upload successful", response);
+              setFilePath(response.filePath);
+            }}
+          />
         </div>
-        
+
       </ImageKitProvider>
     </div>
   );
 }
+//18:18
