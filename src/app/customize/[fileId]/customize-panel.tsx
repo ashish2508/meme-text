18ef8d6fbdef6124d6d0ea43b2dc09eb759b1417
ashish2508 <jha250805@gmail.com>
2025-07-28 19:02:05 +0530

    feat: ✨ text overlay works

--- src/app/customize/[fileId]/customize-panel.tsx
+++ src/app/customize/[fileId]/customize-panel.tsx
@@ -1,23 +1,38 @@
 "use client"
 import { urlEndpoint } from "@/app/providers";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import type { FileObject } from "imagekit/dist/libs/interfaces";
 import { IKImage } from "imagekitio-next";
+import { useState } from "react";
 
 export function CustomizePanel({
   file,
-}:{
-  file: Pick<FileObject,'filePath' | 'name'>
+}: {
+  file: Pick<FileObject, 'filePath' | 'name'>
 }) {
+  const [textOverlay1, setTextOverlay1] = useState<string>("");
+  const transformations = [];
+  if (textOverlay1) {
+    transformations.push({ raw: `l-text,${encodeURIComponent(textOverlay1)},fs-50,l-end` });
+  }
   return (
     <div className="grid grid-cols-2 gap-8">
-      <form action=""></form>
-    <IKImage
-      path={file.filePath}
-      urlEndpoint={urlEndpoint}
-      alt={file.name}
-      width={300}
-      height={800}
-    />
+      <form action="">
+        <Label htmlFor="textOverlay1">Text overlay 1</Label>
+        <Input id="textOverlay1"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTextOverlay1(e.target.value)}
+          value={textOverlay1}
+        />
+      </form>
+      <IKImage
+        path={file.filePath}
+        urlEndpoint={urlEndpoint}
+        alt={file.name}
+        width={300}
+        height={300}
+        transformation={transformations}
+      />
     </div>
   );
 }
