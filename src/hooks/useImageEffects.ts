6f67cde474a3f8b5d80967a0cb39000f75c076eb
ashish2508 <jha250805@gmail.com>
2025-07-29 20:03:29 +0530

    refactor: 🔨 new effects

--- src/hooks/useImageEffects.ts
+++ src/hooks/useImageEffects.ts
@@ -5,15 +5,18 @@ export const useImageEffects = () => {
   const [border, setBorder] = useState(false);
   const [sharpen, setSharpen] = useState(false);
   const [grayscale, setGrayscale] = useState(false);
-
+  const [croprounded, setCropRounded] = useState(false);
+  
   return {
     blur,
     border,
     sharpen,
     grayscale,
+    croprounded,
     setBlur,
     setBorder,
     setSharpen,
     setGrayscale,
+    setCropRounded,
   };
 };
