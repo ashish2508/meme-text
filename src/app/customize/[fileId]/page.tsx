789823a4e91bf271b1724da4644b789b7c7dfb80
ashish2508 <jha250805@gmail.com>
2025-07-31 01:06:52 +0530

    .

--- src/app/customize/[fileId]/page.tsx
+++ src/app/customize/[fileId]/page.tsx
@@ -15,13 +15,6 @@ export default async function CustomizePage({
 
   return (
     <div className="container mx-auto space-y-8 py-8 px-4">
-      <h1
-        className="text-xl font-semibold sm:text-3xl bg-gradient-to-t from-[#ff9a9e] to-[#fecfef] bg-clip-text text-transparent"
-      >
-        Customizing template:
-      </h1>
-
-  
   <p className="text-xl sm:text-xl cursor-pointer font-semibold">
     <a href={`/search?q=${file.customMetadata?.displayName || file.name}`}
           className="text-xl font-bold sm:text-4xl bg-[linear-gradient(to_right,#ff8177_0%,#ff867a_0%,#ff8c7f_21%,#f99185_52%,#cf556c_78%,#b12a5b_100%)] bg-clip-text text-transparent hover:underline hover:text-zinc-400 dark:hover:text-gray-300">
