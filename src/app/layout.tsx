9562de7538a737509a3808bcd001be15969f43bd
ashish2508 <jha250805@gmail.com>
2025-08-01 21:41:26 +0530

    fix: 🐛 dark default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <body className="font-sans antialiased">
         <Providers>
           <Header />
