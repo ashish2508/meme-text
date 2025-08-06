44d984bab5dc6f3bd718a2ddcf3ca29c44c18d65
ashish2508 <jha250805@gmail.com>
2025-08-06 21:53:09 +0530

    feat: ✨ renaming

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,40 +1,51 @@
-'use server'
-
+"use server";
 
 import { db } from "@/app/db/db";
 import { favoriteCounts, favorites } from "@/app/db/schema";
 import { assertAuthenticated } from "@/app/lib/auth-utils";
-import { auth } from "@/auth";
 import { and, eq, sql } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
-export async function toggleFavMemeAction(
+export async function toggleFavoriteMemeAction(
   fileId: string,
   filePath: string,
+  pathToRevalidate: string
 ) {
   const userId = await assertAuthenticated();
-  {
 
-    const favorite = await db.query.favorites.findFirst({
-      where: and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)),
+  const favorite = await db.query.favorites.findFirst({
+    where: and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)),
+  });
+
+  if (favorite) {
+    await db
+      .delete(favorites)
+      .where(and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)));
+    await db
+      .update(favoriteCounts)
+      .set({
+        count: sql`${favoriteCounts.count} - 1`,
+      })
+      .where(eq(favoriteCounts.memeId, fileId));
+  } else {
+    await db.insert(favorites).values({
+      userId,
+      memeId: fileId,
+      filePath: filePath,
     });
-    if (favorite) {
-      await db
-        .delete(favorites)
-        .where(and(eq(favorites.userId, userId), eq(favorites.memeId, fileId)));
-      await db
-        .update(favoriteCounts)
-        .set({
-          count: sql`${favoriteCounts.count} - 1`,
-        })
-        .where(eq(favoriteCounts.memeId, fileId));
-    } else {
-      await db.insert(favorites).values({
-        userId,
+    await db
+      .insert(favoriteCounts)
+      .values({
         memeId: fileId,
-        filePath: filePath,
+        count: 1,
+      })
+      .onConflictDoUpdate({
+        set: {
+          count: sql`${favoriteCounts.count} + 1`,
+        },
+        target: favoriteCounts.memeId,
       });
-    }
   }
-  revalidatePath(`/customize/${fileId}`);
+
+  revalidatePath(pathToRevalidate);
 }
