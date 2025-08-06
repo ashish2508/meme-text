1570f204d820c2e9ce54a2b0c8f936690c79f459
ashish2508 <jha250805@gmail.com>
2025-08-06 21:54:28 +0530

    feat: ✨ "/favorites" work started

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { unstable_noStore } from "next/cache";
+import { UploadMemeButton } from "../search/upload-meme-button";
 
-const favorite = () => {
+
+export default async function FavoritesPage() {
+  unstable_noStore();
+
   return (
-    <div>page</div>
-  )
+    <div className="container mx-auto space-y-8 py-8 px-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-bold">Favorites</h1>
+        <UploadMemeButton />
+      </div>
+    </div>
+  );
 }
-
-export default favorite
