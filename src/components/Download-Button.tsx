e99e4c59d5b9c46636f44e1b1e3856f58f79e0f3
ashish2508 <jha250805@gmail.com>
2025-08-06 19:56:15 +0530

    style: 💅for hover

--- src/components/Download-Button.tsx
+++ src/components/Download-Button.tsx
@@ -20,7 +20,7 @@ export default function DownloadButton() {
     <div className="relative inline-block">
       <div
         onClick={handleDownload}
-        className="relative flex items-center justify-center cursor-pointer w-fit bg-pink-600/40 text-black dark:text-white font-sans border-[3px] border-rose-900/30 px-[15px] py-[10px] hover:bg-white hover:text-black transition-colors duration-300 rounded-2xl shadow-lg disabled:opacity-50 disabled:cursor-not-allowed hover:dark:bg-pink-900/90 hover:dark:text-black hover:dark:border-rose-500/30"
+        className="relative flex items-center justify-center cursor-pointer w-fit bg-pink-600/40 text-black dark:text-white font-sans border-[3px] border-rose-900/30 px-[15px] py-[10px] hover:bg-amber-200/40 hover:text-black scale-80 hover:scale-110 transition-colors duration-300 rounded-2xl shadow-lg disabled:opacity-50 disabled:cursor-not-allowed hover:dark:bg-pink-900/90 hover:dark:text-black hover:dark:border-rose-500/30"
       >
         {/* Button content */}
         <div className="flex items-center gap-2">
