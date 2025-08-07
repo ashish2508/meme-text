e8ebe966d3f09b3b745149824333a88bb9e77376
ashish2508 <jha250805@gmail.com>
2025-08-07 22:08:08 +0530

    feat: ✨ add to favorite button works

--- src/app/search/results-list.tsx
+++ src/app/search/results-list.tsx
@@ -17,44 +17,53 @@ import { urlEndpoint } from "../providers";
 export function ResultsList({
   files,
   counts = [],
-  file,
-  isFavorited,
+  favoritedFiles = [],
+  searchQuery = "",
 }: {
   files: FileObject[];
   counts?: {
     memeId: string;
     count: number;
   }[];
-  file: Pick<FileObject, "filePath" | "name" | "customMetadata" | "width" | "height" | "fileId">;
-  isFavorited: boolean;
+  searchQuery?: string;
+  favoritedFiles?: string[];
 }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-      {files.map((file) => (
-        <Card key={file.fileId}>
-          <CardHeader>
-            <CardTitle className="flex justify-between">
-              <div>{file.customMetadata?.displayName ?? file.name} </div>
-              <div className="flex gap-1 items-center">
-                <FavButton isFavorited={isFavorited} fileId={file.fileId} filePath={file.filePath} pathToRevalidate={`/customize/${file.fileId}`} />
-              </div>
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <IKImage
-              key={file.fileId}
-              path={file.filePath}
-              urlEndpoint={urlEndpoint}
-              alt={file.name}
-            />
-          </CardContent>
-          <CardFooter>
-            <Button asChild>
-              <Link href={`/customize/${file.fileId}`}>Customize</Link>
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+      {files.map((file) => {
+        const isFavorited = favoritedFiles.includes(file.fileId);
+
+        return (
+          <Card key={file.fileId}>
+            <CardHeader>
+              <CardTitle className="flex justify-between">
+                <div>{file.customMetadata?.displayName ?? file.name}</div>
+                <div className="flex gap-1 items-center">
+                  <FavButton
+                    isFavorited={isFavorited}
+                    fileId={file.fileId}
+                    filePath={file.filePath}
+                    pathToRevalidate={`/search?q=${encodeURIComponent(searchQuery)}`}
+                  />
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <IKImage
+                key={file.fileId}
+                path={file.filePath}
+                urlEndpoint={urlEndpoint}
+                alt={file.name}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button asChild>
+                <Link href={`/customize/${file.fileId}`}>Customize</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        );
+      })}
     </div>
   );
 }
