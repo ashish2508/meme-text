0109db923d7404e23aeeb39b88e95aed3f60fe15
ashish2508 <jha250805@gmail.com>
2025-08-08 18:32:42 +0530

    fix:  error:react/no-unescaped-entities

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -25,7 +25,7 @@ export default async function FavoritesPage() {
             className="object-cover"
             alt="a not found image"
           />
-          <p className="text-xl text-gray-300">You didn't liked what you made?</p>
+          <p className="text-xl text-gray-300">{`You didn't liked what you made?`}</p>
           <Button asChild className="w-fit bg-pink-600/40 text-black dark:text-white border-[3px] border-rose-900/30 px-[15px] py-[10px] hover:bg-amber-200/40 hover:text-black scale-80 hover:scale-110 transition-all duration-300 rounded-2xl shadow-lg hover:dark:bg-pink-900/90 hover:dark:text-black hover:dark:border-rose-500/30">
             <Link href="/search?q=">Find some Memes</Link>
           </Button>
